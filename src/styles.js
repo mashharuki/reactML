a06f282e9bac7d764053587226f2dcf1a2087d12
haruki kondo <avp.104.106.107@ae.auone-net.jp>
2021-07-18 20:08:38 +0900

    スタイル用のファイルを追記しました。

---
+++ src/styles.js
@@ -0,0 +1,20 @@
+/**
+ * スタイル用のファイル
+ */
+const styles = {
+  app: {
+    textAlign: 'center'
+  },
+  canvas: {
+    width: 280,
+    height: 280,
+    border: '1px solid silver'
+  },
+  predict: {
+    fontSize: 24,
+    backgroundColor: 'yellow'
+  }
+}
+
+// 外部に公開する
+export default styles;
